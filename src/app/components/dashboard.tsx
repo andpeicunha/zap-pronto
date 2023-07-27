65d44a8aafbd55c4dabfdc4dd0c1d93352678387
Peixoto, Andre <andre.peixoto@trin.ca>
2023-07-27 17:43:18 -0300

    :fire: Structure adjustments

--- src/app/components/dashboard.tsx
+++ src/app/components/dashboard.tsx
@@ -1,54 +1,67 @@
-"use client";
-import React from "react";
-import { useSession } from "next-auth/react";
-import Image from "next/image";
+'use client'
+import React from 'react'
+import { useSession } from 'next-auth/react'
+import Image from 'next/image'
 
-import { Drawer, Tooltip } from "@mui/material";
-import { SheetJs } from "@/app/lib/importExcelSheet";
-import { Sidebar } from "./sidebar";
-import S from "./dashboard.module.scss";
-import { Navbar } from "./navbar";
+import { Drawer, Tooltip } from '@mui/material'
+import { SheetJs } from '@/app/lib/importExcelSheet'
+import { Sidebar } from './sidebar/sidebar'
+import S from './dashboard.module.scss'
+import { Navbar } from './navbar/navbar'
 
 export const Dashboard = () => {
-  const { data: session } = useSession({ required: true });
-  const [open, setOpen] = React.useState(false);
+	const { data: session } = useSession({ required: true })
+	const [open, setOpen] = React.useState(false)
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+	const handleDrawerOpen = () => {
+		setOpen(true)
+	}
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+	const handleDrawerClose = () => {
+		setOpen(false)
+	}
 
-  return (
-    <>
-      <div className={S.container} onClick={open ? handleDrawerClose : () => {}}>
-        <Tooltip title="Abrir Menu" arrow placement="right">
-          <Image
-            src="/images/icon-logo.png"
-            width={93}
-            height={57}
-            alt="Abrir Menu"
-            className={`${S.iconOpenMenu} ${open ? S.hidden : ""}`}
-            onClick={handleDrawerOpen}
-          />
-        </Tooltip>
+	return (
+		<>
+			<div
+				className={S.container}
+				onClick={open ? handleDrawerClose : () => {}}
+			>
+				<Tooltip
+					title='Abrir Menu'
+					arrow
+					placement='right'
+				>
+					<Image
+						src='/images/icon-logo.png'
+						width={93}
+						height={57}
+						alt='Abrir Menu'
+						className={`${S.iconOpenMenu} ${open ? S.hidden : ''}`}
+						onClick={handleDrawerOpen}
+					/>
+				</Tooltip>
 
-        <div className={S.header}>
-          <Navbar />
-        </div>
+				<div className={S.header}>
+					<Navbar />
+				</div>
 
-        <div className={S.main}>
-          Main
-          <SheetJs />
-        </div>
-        <div className={open ? `${S.open}` : `${S.open} ${S.hidden}`}></div>
-      </div>
+				<div className={S.main}>
+					Main
+					<SheetJs />
+				</div>
+				<div className={open ? `${S.open}` : `${S.open} ${S.hidden}`}></div>
+			</div>
 
-      <Drawer variant="persistent" anchor="left" open={open} onClose={handleDrawerClose} className={S.drawerSidebar}>
-        <Sidebar onClick={handleDrawerClose} />
-      </Drawer>
-    </>
-  );
-};
+			<Drawer
+				variant='persistent'
+				anchor='left'
+				open={open}
+				onClose={handleDrawerClose}
+				className={S.drawerSidebar}
+			>
+				<Sidebar onClick={handleDrawerClose} />
+			</Drawer>
+		</>
+	)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,34 @@
-import { getServerSession } from "next-auth";
-import Image from "next/image";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { getServerSession } from 'next-auth'
+import Image from 'next/image'
+import { authOptions } from './api/auth/[...nextauth]/route'
 
-import { Navbar } from "@/app/components/navbar";
-import { Dashboard } from "@/app/components/dashboard";
+import { Navbar } from '@/app/components/navbar/navbar'
+import { Dashboard } from '@/app/components/dashboard'
 
-import S from "./page.module.scss";
+import S from './page.module.scss'
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+	const session = await getServerSession(authOptions)
 
-  if (!session) {
-    return (
-      <main className={S.main}>
-        <Navbar />
-        <Image src="/logotipo.png" width={577} height={82} alt={""} priority className={S.imgLogo} />
-      </main>
-    );
-  }
+	if (!session) {
+		return (
+			<main className={S.main}>
+				<Navbar />
+				<Image
+					src='/logotipo.png'
+					width={577}
+					height={82}
+					alt={''}
+					priority
+					className={S.imgLogo}
+				/>
+			</main>
+		)
+	}
 
-  return (
-    <main className={S.mainLogin}>
-
-      <Dashboard />
-    </main>
-  );
+	return (
+		<main className={S.mainLogin}>
+			<Dashboard />
+		</main>
+	)
 }
