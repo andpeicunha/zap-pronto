fff83a5535b1a3cf6324a131f8719cfb30860246
Andre Peixoto <andrepeixoto@gmail.com>
2024-07-22 21:24:22 -0300

    :white_check_mark: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
 const nextJest = require('next/jest')
-const createJestConfig = nextJest({ dir: './' })
 
-const customJestConfig = {
+/** @type {import('jest').Config} */
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const config = {
+  coverageProvider: 'v8',
   testEnvironment: 'jsdom',
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
 
-module.exports = createJestConfig(customJestConfig)
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(config)
